07bd80495a4a7aef7e55f9f72da6b030c8f40d24
bculleec <bculleec@uoguelph.ca>
2023-12-30 19:43:59 -0500

    added API calls for text descriptions

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const pokemon_names = [];
 const pokemon_urls = [];
+const pokemon_descriptions = [];
 const screen = document.querySelector('.image');
 const display_name = document.querySelector('.pokemon-name');
+const display_description = document.querySelector('.text');
 const url = 'https://pokeapi.co/api/v2/pokedex/2';
 
 
@@ -19,7 +21,13 @@ async function GetPokemonData() {
 
         sprite_url = pokemon_data.sprites.front_default;
         pokemon_urls.push(sprite_url);
-        
+
+        const pokemon_url_desc = `https://pokeapi.co/api/v2/pokemon-species/${ entry.entry_number }/`;
+        const response3 = await fetch(pokemon_url_desc);
+        const pokemon_data2 = await response3.json();
+
+        let desc = pokemon_data2.flavor_text_entries[8].flavor_text;
+        pokemon_descriptions.push(desc);
     })
 }
 
@@ -32,4 +40,5 @@ function GenerateRandom() {
     const num = Math.floor(Math.random() * 151);
     screen.innerHTML = `<img class = "pokemon-image" src = "${ pokemon_urls[num] }">`;
     display_name.innerHTML = pokemon_names[num];
+    display_description.innerHTML = pokemon_descriptions[num];
 }
